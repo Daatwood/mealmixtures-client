df8f81ef654528a9d77d57edb02376d2fb3fee87
Dustin Atwood <dustin@atwood.email>
2021-08-19 18:26:24 -0500

    Hotfix: check for owner prop on recipe

--- src/utils/isOwner.js
+++ src/utils/isOwner.js
@@ -1,6 +1,6 @@
 // TODO: tie recipe ownership to id
 function isOwner(user, recipe) {
-	if (!user || !recipe) return false;
+	if (!user || !recipe || !recipe.owner || !user.token) return false;
 	return user.email === recipe.owner;
 }
 export default isOwner;
